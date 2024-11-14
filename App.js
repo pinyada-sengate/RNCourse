4f560460636ffeef55cea7318187dcd381eb2fb4
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-11-14 15:08:06 +0100

    improve layout

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your course goal" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals... </Text>
       </View>
     </View>
@@ -17,12 +17,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
@@ -30,5 +36,8 @@ const styles = StyleSheet.create({
     width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
